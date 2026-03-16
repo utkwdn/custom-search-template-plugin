6fe09ca9ff528d1b17d672656fba2e35c84b7948
Mark Naylor <marknaylor@utkmachines-MacBook-Pro-9.local>
2026-03-16 10:42:12 -0500

    chore(search): add function doc block

--- custom-search-template-plugin.php
+++ custom-search-template-plugin.php
@@ -4,7 +4,7 @@
  * Description:  Update search page template to remove site search and tabs
  * Plugin URI:   https://github.com/utkwdn/custom-search-template-plugin
  * Author:       The University of Tennessee, Knoxville
- * Version:      0.1.0
+ * Version:      1.1.0
  * Text Domain:  custom-search-template
  * Domain Path:  /languages
  * License:      GPL v2 or later
@@ -18,8 +18,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-add_filter( 'template_include', 'override_search_template' );
-
+/**
+ * Overrides the default WordPress search template with a custom template
+ * provided by this plugin.
+ *
+ * When a search results page is requested, this function checks for a
+ * `search.php` template inside the plugin's `templates` directory. If the
+ * file exists, it is used instead of the active theme's search template.
+ *
+ * Hooked to the `template_include` filter.
+ *
+ * @param string $template Path to the template WordPress intends to load.
+ * @return string Modified template path if the custom template exists,
+ *                otherwise the original template path.
+ */
 function override_search_template( $template ) {
 	if ( is_search() ) {
 		$custom_template = plugin_dir_path( __FILE__ ) . 'templates/search.php';
@@ -31,3 +43,5 @@ function override_search_template( $template ) {
 
 	return $template;
 }
+
+add_filter( 'template_include', 'override_search_template' );
